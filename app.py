25343d0074002a352ed929ecf2a8944603c3be81
cfarro-hub <cfarro@eada.net>
2025-12-15 20:15:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -333,5 +333,5 @@ if st.button("Build portfolios for this strategy"):
         )
 
         st.info(roboadvisor_comment(ret_new, vol_new, sh_new, base_sh, profile))
-else:
-    st.info("Click 'Build portfolios for this strategy' to see portfolio options and customize weights.")
+    else:
+        st.info("Click 'Build portfolios for this strategy' to see portfolio options and customize weights.")
